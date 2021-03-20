66e03a6e14137ec748950afb3a70ff6d90e74669
Ihor Koval <nicholakoval@gmail.com>
2021-03-20 17:09:13 +0200

    happyshps v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { HitComponent } from './components/hit/hit.component';
+import { DescComponent } from './components/desc/desc.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { OrderComponent } from './components/order/order.component';
+import { FormComponent } from './components/form/form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HitComponent,
+    DescComponent,
+    SkillsComponent,
+    CatalogComponent,
+    BenefitsComponent,
+    FeedbackComponent,
+    OrderComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
